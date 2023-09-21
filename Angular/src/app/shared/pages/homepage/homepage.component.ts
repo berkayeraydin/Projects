cf6598b93d4e4a432189e73d25e9f2fed2815652
Berkay ERAYDIN <berkay.eraydin@etiya.com>
2023-09-21 14:57:42 +0300

    fix

--- Angular/src/app/shared/pages/homepage/homepage.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  templateUrl: './homepage.component.html',
-  styleUrls: ['./homepage.component.css']
-})
-export class HomepageComponent {
-
-}
